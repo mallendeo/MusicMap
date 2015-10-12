bd9dd9e5963e17c7db71bca588ff7a65807a3b5b
Mauricio Allende <mauricio.allende.o@gmail.com>
2015-10-12 19:01:12 -0300

    add icons, update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var browserify = require('browserify')
 var buffer     = require('vinyl-buffer')
+var del        = require('del')
 var gulp       = require('gulp')
 var gutil      = require('gulp-util')
 var rename     = require('gulp-rename')
@@ -7,19 +8,27 @@ var source     = require('vinyl-source-stream')
 var stylus     = require('gulp-stylus')
 var uglify     = require('gulp-uglify')
 
-gulp.task('default', ['browserify', 'stylus'])
+gulp.task('default', ['build'])
+
+gulp.task('clean', function () {
+  return del([
+    'dist/**/*',
+  ])
+})
+
+gulp.task('default', ['clean:mobile']);
 
 gulp.task('stylus', function () {
-  gulp.src('./src/youtify.styl')
+  gulp.src('src/youtify.styl')
     .pipe(stylus({
         compress: gutil.env.dist
       }))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('dist'))
 })
 
 gulp.task('browserify', function() {
   var b = browserify({
-    entries: './src/index.js',
+    entries: 'src/index.js',
     debug: true
   })
 
@@ -32,17 +41,27 @@ gulp.task('browserify', function() {
 
 gulp.task('copy-files', function() {
   return gulp.src([
-      'LICENSE',
       'src/manifest.json',
-      'src/inject.js'
-    ])
-    .pipe(gulp.dest('./dist'))
+      'src/inject.js',
+      'src/assets/**/*'
+    ], { base: 'src' })
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('license', function() {
+  return gulp.src('LICENSE')
+    .pipe(gulp.dest('dist'))
 })
 
 gulp.task('watch', ['build'], function() {
-  gulp.watch(['*', 'src/*'], ['copy-files'])
+  gulp.watch([
+    '*',
+    'src/*.json',
+    'src/inject.js',
+    'src/assets/*'
+  ], ['copy-files'])
   gulp.watch('src/**/*.js', ['browserify'])
   gulp.watch('src/**/*.styl', ['stylus'])
 })
 
-gulp.task('build', ['copy-files', 'browserify', 'stylus'])
+gulp.task('build', ['clean', 'copy-files', 'browserify', 'stylus', 'license'])
