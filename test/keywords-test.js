12e1085042c5c420175a16e899b31868a674376c
Mauricio Allende <mauricio.allende.o@gmail.com>
2015-10-12 23:27:42 -0300

    update keywords test

--- test/keywords-test.js
+++ test/keywords-test.js
@@ -9,7 +9,10 @@ describe('Keywords', function() {
   describe('check keywords', function() {
     it('should match the title keywords', function() {
       for (var i = 0; i < keywords.length; i++) {
-        youtify.getKeywords(keywords[i].title).should.equal(keywords[i].keywords)
+        var songInfo = youtify.getInfoFromTitle(keywords[i].title)
+        console.log(songInfo)
+        songInfo = [songInfo.artist, songInfo.title, songInfo.remix].join(' ').trim()
+        songInfo.should.equal(keywords[i].keywords)
       }
     })
   })
