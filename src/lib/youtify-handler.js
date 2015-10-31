f382a70fe8506b25fdc433f77c07a72036a9ca00
Mauricio Allende <mauricio.allende.o@gmail.com>
2015-10-31 19:36:08 -0300

    fix youtify-handler

--- src/lib/youtify-handler.js
+++ src/lib/youtify-handler.js
@@ -33,30 +33,31 @@ YoutifyHandler.prototype.loadGoogleAnalytics = function () {
 };
 
 YoutifyHandler.prototype.loadYoutify = function () {
+  let self = this;
   if (!document.location.href.match(ytRegex)) return;
   ga('send', 'pageview');
 
   let videoTitle = document.querySelector('#eow-title').textContent,
-      songInfo = this.youtify.getInfoFromTitle(videoTitle),
+      songInfo = self.youtify.getInfoFromTitle(videoTitle),
       categoryElems = [].slice.call(document.querySelectorAll('.watch-info-tag-list li a')),
       isMusicCategory = categoryElems.some(elem => {
         return elem.getAttribute('data-ytid') === 'UC-9-kyTW8ZkZNDHQJ6FgpwQ';
       }),
       descriptionElem = document.querySelector('#eow-description'),
       description = descriptionElem.innerHTML,
-      guessYouTubeMusicVideo = this.youtify.guessYouTubeMusicVideo(description),
-      button = this.createButton(['youtify-open-button','disabled']),
-      spotifyUrl = this.youtify.getSpotifyUrlFromDescription(descriptionElem);
+      guessYouTubeMusicVideo = self.youtify.guessYouTubeMusicVideo(description),
+      button = self.createButton(['youtify-open-button','disabled']),
+      spotifyUrl = self.youtify.getSpotifyUrlFromDescription(descriptionElem);
 
   videoTitle = [songInfo.artist, songInfo.title, songInfo.remix].join(' ');
 
-  this.updateButton(button, 'Loading...');
+  self.updateButton(button, 'Loading...');
 
-  if (this.youtify.isMix(description)) {
-    this.updateButton(button, 'Mix not available in Spotify');
+  if (self.youtify.isMix(description)) {
+    self.updateButton(button, 'Mix not available in Spotify');
     if (!spotifyUrl) return;
 
-    this.updateButton(button, 'Open the tracklist on Spotify');
+    self.updateButton(button, 'Open the tracklist on Spotify');
     button.classList.remove('disabled');
     button.href = spotifyUrl;
     button.target = '_blank';
@@ -64,22 +65,22 @@ YoutifyHandler.prototype.loadYoutify = function () {
   }
 
   if (!isMusicCategory && !guessYouTubeMusicVideo) {
-    this.updateButton(button, 'Not in music category');
+    self.updateButton(button, 'Not in music category');
     return;
   }
 
-  this.youtify.search(videoTitle, 'track').then(data => {
+  self.youtify.search(videoTitle, 'track').then(data => {
     if (data.tracks.items && data.tracks.items[0]) {
       button.classList.remove('disabled');
       button.href = data.tracks.items[0].uri;
-      this.updateButton(button, 'Open in Spotify');
+      self.updateButton(button, 'Open in Spotify');
 
       button.addEventListener('click', () => {
         document.querySelector('.video-stream').pause();
-        this.sendButtonClickGa(data.tracks.items[0].uri);
+        self.sendButtonClickGa(data.tracks.items[0].uri);
       });
     } else {
-      this.updateButton(button, 'Not available in Spotify');
+      self.updateButton(button, 'Not available in Spotify');
     }
   });
 };
