b4d46342413523ecb0f55a66037ee8ceba2c01df
Mauricio Allende <mauricio.allende.o@gmail.com>
2016-06-21 13:45:11 -0300

    fix google analytics

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import musicmap from './lib/musicmap';
 import analytics from './lib/analytics';
 import env from './.env.js';
 
-const ga = analytics(env.G_ANALYTICS_CODE);
+const visitor = analytics(env.G_ANALYTICS_CODE);
 let lastTitle = '';
 setInterval(() => {
   const titleElem = document.querySelector('#eow-title');
   if (!titleElem) return;
 
   if (lastTitle !== titleElem.textContent) {
-    musicmap().load(ga);
+    musicmap().load(visitor);
     lastTitle = titleElem.textContent;
   }
 }, 100);
